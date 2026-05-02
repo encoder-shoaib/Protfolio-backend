724b4de54f84a3a6d4c0f5aba9bd75a9a94c8eb9
encoder-shoaib <ahammed-15-4955@diu.edu.bd>
2026-05-02 23:59:28 +0600

    fixed cors link

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://protfolio-frontend-tawny.vercel.app",
+    ],
     credentials: true,
   }),
 );
